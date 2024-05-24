3a5bf5d858cc1fb54b47d8bf251fd15339c742b3
minkyeong <wlwn2382d@naver.com>
2024-05-24 16:20:10 +0900

    test3

--- src/components/reviewcom/ReviewList.jsx
+++ src/components/reviewcom/ReviewList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ImageDisplay from './ImageDisplay';
+import ImageDisplay from './ImageDIsplay';
 
 const ReviewList = ({ contentId }) => {
   const [reviewData, setReviewData] = useState([]);
